ea4aaab2e1e7923b8189f904ae509cd836cd189e
OddoAkbar <oddo.190@hotmail.com>
2023-01-17 22:02:10 -0300

    tweak(ui): add animation to Arrow in accordion component

--- web/src/lib/components/accordion/index.tsx
+++ web/src/lib/components/accordion/index.tsx
@@ -18,7 +18,7 @@ export const Accordion = ({ title, children, ...rest }: AccordionProps) => {
   return (
     <div className="w-full px-2 py-1 bg-neutral-800 cursor-pointer" {...rest}>
       <div className="flex items-center gap-2 text-neutral-200" onClick={handleChangeExpanded}>
-        {expanded ? <ArrowDownIcon className="w-3 h-3" /> : <ArrowRightIcon className="w-3 h-3" />}
+        <ArrowRightIcon className={`w-3 h-3 transition-transform ${expanded ? 'rotate-90' : 'rotate-0'}`} />
         <span className="text-sm">{title}</span>
       </div>
 
